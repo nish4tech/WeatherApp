3e887b9f76d27b225a3ec560487d0daae8a8a0a1
nishant4tech <nish4tech@gmail.com>
2024-07-29 09:47:21 -0400

    first commit

---
+++ app/src/main/java/com/nish/weatherapp/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.nish.weatherapp
+
+import android.app.Application
+
+class MainApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //todo create Koin and dependencies
+    }
+}
